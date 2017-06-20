918e0089d33a3ab88074630b604557d4c49334b8
Daniel Burke <dcb6@rice.edu>
2017-06-19 23:09:56 -0500

    Working checkbox function for accelerometer and quaternions.

--- CS Template/DeviceSetup.xaml.cs
+++ CS Template/DeviceSetup.xaml.cs
@@ -36,11 +36,25 @@ namespace MbientLab.MetaWear.Template {
         /// Pointer representing the MblMwMetaWearBoard struct created by the C++ API
         /// </summary>
         private IntPtr cppBoard;
-        
+        Val val1 = new Val();
+
         public DeviceSetup() {
             this.InitializeComponent();
+            //bindData();
+        }
+
+
+        public class Val
+        {
+            public string quatString = "";
         }
         
+        void bindData()
+        {
+            orientationText.DataContext = val1.quatString;
+        }
+
+
         protected override void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
 
@@ -59,19 +73,20 @@ namespace MbientLab.MetaWear.Template {
             this.Frame.Navigate(typeof(MainPage));
         }
 
-        //private Fn_IntPtr accDataHandler = new Fn_IntPtr(pointer =>
-        //{
-        //    System.Diagnostics.Debug.WriteLine("This should be working");
-        //    Data data = Marshal.PtrToStructure<Data>(pointer);
-        //    System.Diagnostics.Debug.WriteLine(Marshal.PtrToStructure<CartesianFloat>(data.value));
-        //});
+        private Fn_IntPtr accDataHandler = new Fn_IntPtr(pointer =>
+        {
+            Data data = Marshal.PtrToStructure<Data>(pointer);
+            System.Diagnostics.Debug.WriteLine("A: " + DateTime.Now + " " + Marshal.PtrToStructure<CartesianFloat>(data.value));
+        });
 
         //NEW
-       private Fn_IntPtr fusionAccHandler = new Fn_IntPtr(FusionAccDataPtr =>
+
+       private Fn_IntPtr fusionDataHandler = new Fn_IntPtr(FusionAccDataPtr =>
        {
        
             Data marshalledData = Marshal.PtrToStructure<Data>(FusionAccDataPtr);
-            System.Diagnostics.Debug.WriteLine(DateTime.Now + " Fusion  " + Marshal.PtrToStructure<Quaternion>(marshalledData.value));
+            System.Diagnostics.Debug.WriteLine("Q: " + DateTime.Now + " " + Marshal.PtrToStructure<Quaternion>(marshalledData.value));
+            //val1.quatString = Marshal.PtrToStructure<Quaternion>(marshalledData.value);
             //sSystem.Diagnostics.Debug.WriteLine(marshalledData);
 
            //TextBlock textBlock1 = new TextBlock();
@@ -80,7 +95,6 @@ namespace MbientLab.MetaWear.Template {
            // Send(message);
        });
         //ENDNEW
-
         //public static void SetReadingText(TextBlock textBlock, Data marshalledData)
         //{
         //    textBlock.Text = string.Format(DateTime.Now + " Fusion  " + Marshal.PtrToStructure<Quaternion>(marshalledData.value));
@@ -95,44 +109,58 @@ namespace MbientLab.MetaWear.Template {
         //}
 
 
-        private void accStart_Click(object sender, RoutedEventArgs e)
+        private void Start_Click(object sender, RoutedEventArgs e)
         {
             ///NEW
-            mbl_mw_settings_set_connection_parameters(cppBoard, 7.5F, 7.5F, 0, 6000);
-            mbl_mw_sensor_fusion_set_mode(cppBoard, SensorFusion.Mode.NDOF);
-            mbl_mw_sensor_fusion_set_acc_range(cppBoard, SensorFusion.AccRange.AR_16G); ///AR_2G, 4, 8, 16
-            mbl_mw_sensor_fusion_set_gyro_range(cppBoard, SensorFusion.GyroRange.GR_2000DPS); ///GR_2000DPS, 1000, 500, 250
+            ///
+            if (quaternionCheckBox.IsChecked == true )
+            {
+                mbl_mw_settings_set_connection_parameters(cppBoard, 7.5F, 7.5F, 0, 6000);
+                mbl_mw_sensor_fusion_set_mode(cppBoard, SensorFusion.Mode.NDOF);
+                mbl_mw_sensor_fusion_set_acc_range(cppBoard, SensorFusion.AccRange.AR_16G); ///AR_2G, 4, 8, 16
+                mbl_mw_sensor_fusion_set_gyro_range(cppBoard, SensorFusion.GyroRange.GR_2000DPS); ///GR_2000DPS, 1000, 500, 250
 
-            mbl_mw_sensor_fusion_write_config(cppBoard);
+                mbl_mw_sensor_fusion_write_config(cppBoard);
 
-            IntPtr fusionsAccSignal = mbl_mw_sensor_fusion_get_data_signal(cppBoard, SensorFusion.Data.QUATERION); //this line works
+                IntPtr fusionsDataSignal = mbl_mw_sensor_fusion_get_data_signal(cppBoard, SensorFusion.Data.QUATERION); //this line works
 
-            mbl_mw_datasignal_subscribe(fusionsAccSignal, fusionAccHandler);
-            mbl_mw_sensor_fusion_enable_data(cppBoard, SensorFusion.Data.QUATERION);
-            mbl_mw_sensor_fusion_start(cppBoard);
+                mbl_mw_datasignal_subscribe(fusionsDataSignal, fusionDataHandler);
+                mbl_mw_sensor_fusion_enable_data(cppBoard, SensorFusion.Data.QUATERION);
+                mbl_mw_sensor_fusion_start(cppBoard);
+            }
 
-            
+            if (accelerometerCheckBox.IsChecked == true)
+            {
+                IntPtr accSignal = mbl_mw_acc_get_acceleration_data_signal(cppBoard);
+
+                mbl_mw_datasignal_subscribe(accSignal, accDataHandler);
+                mbl_mw_acc_enable_acceleration_sampling(cppBoard);
+                mbl_mw_acc_start(cppBoard);
+            }
+      
             //ENDNEW
-
-
-            //IntPtr accSignal = mbl_mw_acc_get_acceleration_data_signal(cppBoard);
-
-            //mbl_mw_datasignal_subscribe(accSignal, accDataHandler);
-            //mbl_mw_acc_enable_acceleration_sampling(cppBoard);
-            //mbl_mw_acc_start(cppBoard);
         }
 
-        private void accStop_Click(object sender, RoutedEventArgs e)
+        private void Stop_Click(object sender, RoutedEventArgs e)
         {
             ///NEW
-            mbl_mw_sensor_fusion_stop(cppBoard);
+            ///
+            if (quaternionCheckBox.IsChecked == true)
+            {
+                mbl_mw_sensor_fusion_stop(cppBoard);
+            }
             ///ENDNEW
 
-            //IntPtr accSignal = mbl_mw_acc_get_acceleration_data_signal(cppBoard);
+            if (accelerometerCheckBox.IsChecked == true)
+            {
+                IntPtr accSignal = mbl_mw_acc_get_acceleration_data_signal(cppBoard);
 
-            //mbl_mw_acc_stop(cppBoard);
-            //mbl_mw_acc_disable_acceleration_sampling(cppBoard);
-            //mbl_mw_datasignal_unsubscribe(accSignal);
+                mbl_mw_acc_stop(cppBoard);
+                mbl_mw_acc_disable_acceleration_sampling(cppBoard);
+                mbl_mw_datasignal_unsubscribe(accSignal);
+            }
+
+
         }
     }
 }
